959a95196d177127ca9f6521cade7e99fe3ac4a9
mayukh <mayukhchatterjee7029@gmail.com>
2025-10-28 14:05:42 +0530

    new commit

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -5,7 +5,7 @@ import { icons } from '@/constants/icons'
 
 const SearchBar = () => {
     return (
-        <View className='flex-row items-center bg-dark-200 rounded-full px-5 py-4' >
+        <View className='flex-row items-center bg-dark-200 rounded-full px-5 py-3' >
             <Image source={icons.search} className='size-5' resizeMode='contain' tintColor={"#ab8bff"} />
             <TextInput
                 onPress={() => { }}
